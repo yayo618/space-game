fdb65a411ba7283872337961640f79817a961f38
yayo <jorgefloresari@gmail.com>
2022-12-13 11:07:34 -0500

    player creation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-alert(c)
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
