b73449fc0ecc28da7a28233dd674a11dc3cc2afb
yayo <jorgefloresari@gmail.com>
2022-12-18 20:01:10 -0500

    platforms

--- index.js
+++ index.js
@@ -35,7 +35,20 @@ class Player {
     }
 }
 
+class Platform {
+    constructor() {
+        this.position = {x: 100, y: 200}
+        this.width = 200
+        this.height = 20
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
 const keys = {
     right: {pressed: false},
     left: {pressed: false}
@@ -45,10 +58,20 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {player.velocity.x = 5}
     else if (keys.left.pressed) {player.velocity.x = -5}
     else {player.velocity.x = 0}
+
+    //collision
+    if (
+        player.position.y + player.height <= platform.position.y &&
+        player.position.y + player.height + player.velocity.y >= platform.position.y &&
+        player.position.x + player.width >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.width
+    ) 
+    {player.velocity.y = 0}
 }
 animate()
 
