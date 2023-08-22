2076b57aed0dfaa69d2821977dd49847e268d1b0
yayo <jorgefloresari@gmail.com>
2023-08-22 16:58:17 -0500

    adding red colider and improve level

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var i_hill = new Image()
 i_hill.src = 'assets/hill.png'
 var i_smallT = new Image()
 i_smallT.src = 'assets/platformSmallTall.png'
+var i_enemyL = new Image()
+i_enemyL.src = 'assets/enemy_1_left.png'
 
 var i_sRL = new Image()
 var i_sRR = new Image()
@@ -19,6 +21,9 @@ i_sRL.src = 'assets/sRunL.png'
 i_sRR.src = 'assets/sRunR.png'
 i_sSL.src = 'assets/sSL.png'
 i_sSR.src = 'assets/sSR.png'
+
+var i_red_c = new Image()
+    i_red_c.src = 'assets/red-colider.png'
 
 const gravity = 0.5
 
@@ -36,6 +41,48 @@ const sound_jump = new Audio('sounds/cartoon-jump-6462.mp3')
 const track = audioCtx.createMediaElementSource(sound_jump)
 track.connect(audioCtx.destination)
 */
+class Platform {
+    constructor({x, y}, img, width, height) {
+        this.position = {x: x, y: y}
+        this.sprite = img
+        this.width = width
+        this.height = height
+    }
+    draw() {
+        c.drawImage(this.sprite, this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+class GenericObject extends Platform {
+    constructor({x, y}, img, width, height) {
+        super({x, y}, img, width, height)
+    }
+}
+
+class Enemy_collider extends Platform {
+    constructor({x, y}, img, width, height) {
+        super({x, y}, img, width, height)
+    }
+}
+
+class Enemy extends Platform {
+    constructor({x, y}, img, width, height) {
+        super({x, y}, img, width, height)
+    }
+    draw() {
+        c.drawImage(
+            this.sprite, 
+            160,
+            0,
+            160,
+            119,
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+    }
+}
 
 class Player {
     constructor() {
@@ -81,23 +128,6 @@ class Player {
     }
 }
 
-class Platform {
-    constructor({x, y}, img, width, height) {
-        this.position = {x: x, y: y}
-        this.img = img
-        this.width = width
-        this.height = height
-    }
-    draw() {
-        c.drawImage(this.img, this.position.x, this.position.y, this.width, this.height)
-    }
-}
-
-class GenericObject extends Platform {
-    constructor({x, y}, img, width, height) {
-        super({x, y}, img, width, height)
-    }
-}
 
 const keys = {
     right: {pressed: false},
@@ -108,21 +138,35 @@ const keys = {
 let player = new Player()
 let platforms = []
 let genericObj = []
+let enemies = []
+let red_col = []
 let scrollOffset = 0
 
 function init() {
     player = new Player()
     platforms = [
-        new Platform({x: 100, y: 300}, i_platform, 580, 125), 
-        new Platform({x: 400, y: 400}, i_platform, 580, 125),
-        new Platform({x: 1610, y: 370}, i_smallT, 291, 227),
-        new Platform({x: 1611, y: 535}, i_platform, 290, 62),
-        new Platform({x: 1900, y: 535}, i_platform, 290, 62),
-        new Platform({x: 1120, y: 300}, i_platform, 290, 62)
+        new Platform({x: 0, y: 475}, i_platform, 580, 125), 
+        new Platform({x: 579, y: 475}, i_platform, 580, 125),
+        new Platform({x: 1886, y: 375}, i_smallT, 291, 227),
+        new Platform({x: 2277, y: 250}, i_smallT, 291, 227),
+        new Platform({x: 1409, y: 475}, i_platform, 580, 125),
+        new Platform({x: 1988, y: 475}, i_platform, 580, 125),
+        new Platform({x: 2923, y: 475}, i_platform, 580, 125)
     ]
     genericObj = [
-        new GenericObject({x: 0, y: 15}, i_hill, 550, 582),
-        new GenericObject({x: 1000, y: 15}, i_hill, 550, 582)
+        new GenericObject({x: 0, y: 18}, i_hill, 550, 582),
+        new GenericObject({x: 1000, y: 18}, i_hill, 550, 582),
+        new GenericObject({x: 2000, y: 18}, i_hill, 550, 582)
+    ]
+    enemies = [
+        new Enemy({x: 1968, y: 316}, i_enemyL, 80, 59),
+        new Enemy({x: 2408, y: 416}, i_enemyL, 80, 59)
+    ]
+    red_col = [
+        new Enemy_collider({x: 1826, y: 315}, i_red_c, 60, 60), 
+        new Enemy_collider({x: 2177, y: 315}, i_red_c, 60, 60), 
+        new Enemy_collider({x: 2217, y: 415}, i_red_c, 60, 60),
+        new Enemy_collider({x: 2568, y: 415}, i_red_c, 60, 60) 
     ]
     scrollOffset = 0
 }
@@ -135,6 +179,8 @@ function animate() {
 
     genericObj.forEach( (obj) => {obj.draw()})
     platforms.forEach( (platform) => {platform.draw()} )
+    enemies.forEach( (en) => {en.draw()} )
+    red_col.forEach( (rc) => {rc.draw()} )
     player.update()
 
     //collision
@@ -153,7 +199,7 @@ function animate() {
     gestiona_m ()
     gestiona_s()
 
-    if (scrollOffset > 1500) {
+    if (scrollOffset > 2900) {
         c.font = '35px Arial'
         c.fillStyle = 'red'
         c.fillText('you win', 380, 300)
@@ -182,7 +228,7 @@ window.addEventListener('keydown', keyDownUp)
 window.addEventListener('keyup', keyDownUp)
 
 function gestiona_m () {
-    if (keys.right.pressed && player.position.x < (700 - player.width)) {player.velocity.x = player.speed}
+    if (keys.right.pressed && player.position.x < (500 - player.width)) {player.velocity.x = player.speed}
     else if (
         (keys.left.pressed && player.position.x > 100) || 
         (keys.left.pressed && scrollOffset === 0 && player.position.x > 0 )) {player.velocity.x = -player.speed}
@@ -192,10 +238,14 @@ function gestiona_m () {
             scrollOffset += player.speed
             platforms.forEach( (platform) => {platform.position.x -= player.speed} )
             genericObj.forEach( (obj) => {obj.position.x -= player.speed * 0.6} )
+            enemies.forEach( (en) => {en.position.x -= player.speed} )
+            red_col.forEach( (rc) => {rc.position.x -= player.speed} )
         } else if (keys.left.pressed && scrollOffset > 0) {
             scrollOffset -= player.speed
             platforms.forEach( (platform) => {platform.position.x += player.speed} )
             genericObj.forEach( (obj) => {obj.position.x += player.speed * 0.6} )
+            enemies.forEach( (en) => {en.position.x += player.speed} )
+            red_col.forEach( (rc) => {rc.position.x += player.speed} )
         }
     }
     if (keys.jump.pressed) {
